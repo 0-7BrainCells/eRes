9f4f2b2e34a7179f626bec31ea8cfe23fc59d7fe
Henry Goodman <13032204@student.uts.edu.au>
2021-04-04 22:37:04 +1000

    Updating staff schema
    Staff schema just has id and password atm

--- model/Staff.js
+++ model/Staff.js
@@ -3,14 +3,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const staffSchema = new Schema({
-    fname: {
-      type: String,
-      required: true
-    },
-    lname: {
-      type: String,
-      required: true
-    },
     ID: {
       type: String,
       required: true
--- routes/pages.js
+++ routes/pages.js
@@ -15,14 +15,17 @@ router.get('/CustomerRegistration', (req, res) => {
 }), 
 
 router.post('/staff-login-received', (req, res) => {
-  var myData = new Staff(req.body);
-  myData.save()
-     .then(item => {
-          res.send("User saved to database (TODO: direct to login page");
-  })
-  .catch(err => {
-          res.status(400).send("Unable to save to database");
-  });  
+  Staff.findOne({
+    ID: req.body.ID,
+    password: req.body.password
+  }, function(err, user) {
+    if (err) { return res.status(500).send(err); }
+
+    if (!user) { return res.status(200).send("User not found, check username and password are correct"); } //The email or password dont exist in the DB
+
+    return res.status(200).send("You are logged in succesfully. (TODO: actually make some sort of session thing with profiles");
+  }
+)
 }), 
 
 router.post('/customer-login-received', (req, res) => {  //Check if the email and password combo are in the DB to verify login
