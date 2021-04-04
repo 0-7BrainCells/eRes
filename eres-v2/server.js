79eaaaba0c83a938f2bcae8b5f7cba85327d54b3
Henry Goodman <79623665+henrygoodman@users.noreply.github.com>
2021-04-04 12:11:27 +1000

    Update server.js

--- eres-v2/server.js
+++ eres-v2/server.js
@@ -8,6 +8,7 @@ const app = express()
 // })
 //creating the db w the link instead of localhost like the tut
 //copied from Henry's code
+//Henry NOTE: yeah this one just connects to the cloud setup db instead of a local db because it will be easier to work with between all our machines. should have been emailed some invite link online
 mongoose.connect('mongodb+srv://admin:<eres>@eres.k9zxh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', () => {
 
     console.log('Connected to MongoDB Successfully!')
