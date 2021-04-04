fd877a00f28b46a772bd2c4b0f5429929728a886
Henry Goodman <13032204@student.uts.edu.au>
2021-04-04 22:08:35 +1000

    s

--- model/User.js
+++ model/User.js
@@ -30,24 +30,5 @@ const userSchema = new Schema({
   }
 });
 
-const staffSchema = new Schema({
-  fname: {
-    type: String,
-    required: true
-  },
-  lname: {
-    type: String,
-    required: true
-  },
-  ID: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-});
 
 module.exports = mongoose.model('User', userSchema);
-module.exports = mongoose.model('Staff', staffSchema);
--- routes/pages.js
+++ routes/pages.js
@@ -15,17 +15,14 @@ router.get('/CustomerRegistration', (req, res) => {
 }), 
 
 router.post('/staff-login-received', (req, res) => {
-	Staff.findOne({
-    ID: req.body.ID,
-    password: req.body.password
-  }, function(err, user) {
-    if (err) { return res.status(500).send(err); }
-
-    if (!user) { return res.status(200).send("Staff member not found, check username and password are correct"); } //The email or password dont exist in the DB
-
-    return res.status(200).send("You are logged in succesfully to staff. (TODO: actually make some sort of session thing with profiles");
-  }
-)
+  var myData = new Staff(req.body);
+  myData.save()
+     .then(item => {
+          res.send("User saved to database (TODO: direct to login page");
+  })
+  .catch(err => {
+          res.status(400).send("Unable to save to database");
+  });  
 }), 
 
 router.post('/customer-login-received', (req, res) => {  //Check if the email and password combo are in the DB to verify login
