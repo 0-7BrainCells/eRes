0d83987259287d539310f394cddd8e0d0151b515
Henry Goodman <13032204@student.uts.edu.au>
2021-04-04 20:35:45 +1000

    npm install bcrypt

--- server.js
+++ server.js
@@ -1,42 +1,31 @@
 const express = require('express')
-const mongoose = require('mongoose')
-const pageRouter = require('./routes/pages')
+const routes = require('./routes/pages')
 const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
-// mongoose.connect('mongodb://localhost/eRes', {
-//     useNewUrlParser: true, useUnifiedTopology: true
-// })
-//creating the db w the link instead of localhost like the tut
-//copied from Henry's code
-//Henry NOTE: this one just connects to the cloud setup db instead of a local db because it will be easier to work with between all our machines. should have been emailed some invite link online
-mongoose.connect('mongodb+srv://admin:<eres>@eres.k9zxh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', () => {
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://admin:admin@eres.k9zxh.mongodb.net/eRes?retryWrites=true&w=majority', () => {
     console.log('Connected to MongoDB Successfully!')
-
 })
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs') 
 
-app.use('/pages', pageRouter)
-app.use(express.urlencoded({ extended: false }))
+app.use(routes)
 
 app.get('/', (req, res) => {
     const pages = [{
         title: 'eResto Registration', 
         createdAt: new Date()
-        //createAt: Date.now()
-        //decription: 'Test description'
 },
 {
         title: 'eResto Staff Login', 
         createdAt: new Date()
-        //decription: 'Test description'
     }]
-    res.render('pages/index', { pages: pages}) //9:30 in video { pages: pages}, {text: 'Hello'}
+    res.render('index', { pages: pages}) 
 })
-
-
-app.post("/login")
-
 
 app.listen(5000)
