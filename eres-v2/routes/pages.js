6f2f30496ae75683c9a1aa1c1e5f6d95bfcb1197
Rima Nigudkar <Rima.Nigudkar@student.uts.edu.au>
2021-04-04 11:57:15 +1000

    copy contents from JasDev to RimaDev

---
+++ eres-v2/routes/pages.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const Page = require('./../models/page')
+const router = express.Router()
+
+router.get('/StaffLogin', (req, res) => {
+    // res.send('In pages')
+    res.render('pages/staff-login')
+}), 
+router.get('/CustomerLogin', (req, res) => {
+    // res.send('In pages')
+    res.render('pages/customer-login')
+}), 
+router.get('/CustomerRegistration', (req, res) => {
+    // res.send('In pages')
+    res.render('pages/customer-rego')
+})
+router.get('/LunchMenu', (req, res) => {
+    // res.send('In pages')
+    res.render('pages/lunch-menu')
+})
+
+router.post('/', (req, res) =>{
+const page = new Page({
+})
+}
+
+)
+
+module.exports = router 
