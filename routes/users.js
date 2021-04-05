7158fb7189dda31054ea5fee42fdeffffa1d97e0
Henry Goodman <13032204@student.uts.edu.au>
2021-04-05 23:58:40 +1000

    Deleted some comments

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ router.post('/customer-login-received', UserController.customer_login_post)
 router.post('/customer-register-received', UserController.customer_register_post)
 
 router.post('/staff-login-received', UserController.staff_login_post)
-    
+
+
 
 module.exports = router;
