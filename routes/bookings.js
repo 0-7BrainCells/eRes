2b819bc1967ff038d6489f92bd5e6bd020ed409d
Henry Goodman <13032204@student.uts.edu.au>
2021-04-06 16:47:33 +1000

    Editing submit button

--- routes/bookings.js
+++ routes/bookings.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const BookingController = require('../controllers/BookingController')
 
-router.post('/booking-received', BookingController.add_booking)
+router.post('/booking-received', BookingController.add_booking) 
 
 module.exports = router;
