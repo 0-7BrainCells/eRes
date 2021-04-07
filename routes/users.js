389ec6e462f4e42784c713c2ffa74087d40e0906
Henry Goodman <13032204@student.uts.edu.au>
2021-04-07 21:44:46 +1000

    removed another bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const bcrypt = require('bcrypt');
 const UserController = require('../controllers/UserController')
 
 router.post('/customer-login-received', UserController.customer_login_post)
