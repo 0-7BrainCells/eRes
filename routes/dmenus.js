ad458ea36266d6fc9d0ab3316ea0e03f2c9bb526
Henry Goodman <13032204@student.uts.edu.au>
2021-04-08 13:21:58 +1000

    Add pages

--- routes/dmenus.js
+++ routes/dmenus.js
@@ -2,5 +2,6 @@ const express = require('express')
 const router = express.Router()
 const DinnerMenuController = require('../controllers/DinnerMenuController')
 
-router.post('/add-dmenu-item', DinnerMenuController.add_dmenu_item) 
+router.post('/add-dinner-menu-item', DinnerMenuController.add_dmenu_item) 
+
 module.exports = router
