6ea8ff842d7af3ca9cd1ba8717ad1422ce20e16c
Henry Goodman <13032204@student.uts.edu.au>
2021-04-08 13:10:10 +1000

    Added shitty pages for adding menu items

--- routes/pages.js
+++ routes/pages.js
@@ -9,9 +9,6 @@ router.get('/StaffLogin', (req, res) => {
 router.get('/CustomerLogin', (req, res) => {
     res.render('index/customer-login')
 }), 
-router.get('/Customer', (req, res) => {
-  res.render('index/customer-login')
-}), 
 
 router.get('/CustomerRegistration', (req, res) => {
     res.render('index/customer-rego')
@@ -47,11 +44,31 @@ router.get('/StaffLayout', (req, res) => {
 }),
 
 router.get('/EditCustomer', (req, res) => {
-  res.render('staff/admin/edit-customer');
+  res.render('staff/admin/manage-staff-customers/edit-customer');
 }),
 
 router.get('/RemoveStaff', (req, res) => {
-  res.render('staff/admin/remove-staff')
+  res.render('staff/admin/manage-staff-customers/remove-staff')
+}),
+
+router.get('/EditMenu', (req, res) => {
+  res.render('staff/admin/edit-menu')
+}),
+
+router.get('/AddLunchMenuItem', (req, res) => {
+  res.render('staff/admin/edit-menu/add-lunch-menu-item')
+}),
+
+router.get('/RemoveLunchMenuItem', (req, res) => {
+  res.render('staff/admin/edit-menu/remove-lunch-menu-item')
+}),
+
+router.get('/AddDinnerMenuItem', (req, res) => {
+  res.render('staff/admin/edit-menu/add-dinner-menu-item')
+}),
+
+router.get('/RemoveDinnerMenuItem', (req, res) => {
+  res.render('staff/admin/edit-menu/remove-dinner-menu-item')
 }),
 
 router.post('/', (req, res) =>{
