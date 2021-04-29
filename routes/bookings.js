ffa33ad9d4ba24d9e3b6b74636234f8af414e667
spurups <79547654+spurups@users.noreply.github.com>
2021-04-30 05:10:24 +1000

    pt 2

--- routes/bookings.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const BookingController = require('../controllers/BookingController')
-
-router.post('/booking-received', BookingController.add_booking) 
-
-
-module.exports = router;
--- routes/discounts.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const DiscountController = require('../controllers/DiscountController')
-
-router.post('/discount-added', DiscountController.add_discount)
-
-router.post('/discount-removed', DiscountController.remove_discount)
-
-module.exports = router;
