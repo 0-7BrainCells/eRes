dcf27bbbedaca1cbcfbe097087ebc4ee4c84b9e5
Henry Goodman <13032204@student.uts.edu.au>
2021-04-29 13:17:41 +1000

    Categorising menu fully works now

--- routes/dmenus.js
+++ routes/dmenus.js
@@ -5,10 +5,9 @@ const OrderController = require('../controllers/OrderController')
 
 
 router.post('/add-dinner-menu-item', DinnerMenuController.add_dinner_menu_item) 
-
 router.post('/remove-dinner-menu-item', DinnerMenuController.remove_dinner_menu_item) 
-
 router.post('/add-order-dinner', OrderController.add_order_dinner)
+router.post('/categorise-dinner-menu', DinnerMenuController.display_dinner_menu)
 
 router.get('/DinnerMenu', checkAuthenticated, DinnerMenuController.display_dinner_menu)
 
