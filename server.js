bd11f492530091d290f1112d24627c525b179563
Henry Goodman <13032204@student.uts.edu.au>
2021-04-30 19:25:40 +1000

    heroku config

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.set('view engine', 'ejs')
 
 app.use(flash())
 app.use(session({
-    secret: "secretKey",
+    secret: process.env.SESSION_SECRET,
     resave: false, 
     saveUninitialized: false
 }))
